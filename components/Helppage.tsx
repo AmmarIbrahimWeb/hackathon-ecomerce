0af7b5eb6e53803c21ed1461c7e0fe090d79b9c6
MaviaIbrahim <maviaibrahimmalick@gmailcom>
2024-12-30 19:47:33 +0500

    Remove unused HelpPage component

---
+++ components/Helppage.tsx
@@ -0,0 +1,11 @@
+const HelpPage = () => {
+    return (
+        <div className="help-container">
+            <div className="form text-center my-6">
+                <span className="text-2xl font-semibold my-6 uppercase text-center">Get Help</span>
+            </div>
+        </div>
+    );
+};
+
+export default HelpPage;
